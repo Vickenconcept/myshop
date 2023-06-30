e467e482b52d4fb26da8901d99144d1f277b0c36
vickenconcept <vickenconcept@gmail.com>
2023-06-30 19:17:54 +0100

    toggle button added to card

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="px-20 py-10 font-sans">
-        <div class="flex flex-row justify-between pt-5 pb-3">
-            <div class="text-gray-700 text-2xl font-semibold ">Shop</div>
+    <div class="pl-10 md:px-20 py-10 font-sans">
+        <div class="flex flex-col sm:flex-row  justify-between  pt-5 pb-3">
+            <div class="text-gray-700 text-2xl font-semibold pb-3 ">Shop</div>
             <div class="" x-data="{ isOpen: false}">
-                <a type="button" class="bg-blue-700 px-4 py-2 ml-1 rounded-md cursor-pointer text-white" x-on:click="isOpen = true">Categories</a>
-                <a type="button"  href="{{ route('products.create') }}" class="bg-blue-700 px-4 py-2 ml-1 rounded-md text-white">Add New Product</a>
+                <a type="button" class="bg-blue-700 px-4 py-2 ml-1 rounded-md cursor-pointer text-white my-1 " x-on:click="isOpen = true">Categories</a>
+                <a type="button"  href="{{ route('products.create') }}" class="bg-blue-700 px-4 py-2 ml-1 my-1 rounded-md text-white">Add New Product</a>
                 <!-- modal -->
                 <x-landing.my-modal>
                     <x-slot name="title">
@@ -42,7 +42,7 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 bg-white rounded-md py-20 px-10">
             @foreach ($products as $product)
             <!-- <a href="{{ route('products.edit', $product) }}"> -->
-                <x-card :product="$product" url="'{{ route('products.edit', $product) }}'"/>
+                <x-card :product="$product" url="{{ route('products.edit', $product)}}"/>
             <!-- </a> -->
             @endforeach
         </div>
